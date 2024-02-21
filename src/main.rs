f0aad2023547628c9429a7a8ea15a87e11ec5c75
tippfehlr <tippfehlr@tippfehlr.eu>
2024-02-21 18:42:54 +0100

    feat: multiple accounts, test cargo-shuttle

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
-use actix_web::{
-    post,
-    web::{Data, Json},
-    App, HttpRequest, HttpResponse, HttpServer, Responder,
-};
+use actix_web::{post, web, HttpResponse, Responder};
+use chrono::{Duration, Utc};
 use serde::Deserialize;
-
-#[derive(Clone)]
-struct Env {
-    discord_webhook: String,
-    wumpus_token: String,
-    webhook_body: String,
-}
+use shuttle_actix_web::ShuttleActixWeb;
 
 #[allow(non_snake_case, dead_code)]
 #[derive(Deserialize, Debug)]
@@ -21,87 +12,69 @@ struct WumpusWebhook {
     query: serde_json::Value,
 }
 
+#[derive(Deserialize)]
+struct Query {
+    webhook: String,
+}
+
 #[post("/")]
 async fn handle_webhook(
-    env: Data<Env>,
-    data: Json<WumpusWebhook>,
-    req: HttpRequest,
+    query: web::Query<Query>,
+    data: web::Json<WumpusWebhook>,
 ) -> impl Responder {
-    println!("received request");
-    if let Some(auth) = req.headers().get("Authorization") {
-        if auth.to_str().unwrap() == env.wumpus_token {
-            let client = reqwest::Client::new();
-            let body = env
-                .webhook_body
-                .replace("{{userId}}", data.userId.as_str())
-                .replace("{{botId}}", data.botId.as_str());
+    println!("request received");
 
-            match client
-                .post(&env.discord_webhook)
-                .body(body)
-                .header("content-type", "application/json")
-                .send()
-                .await
-            {
-                Ok(res) => {
-                    println!("{:?}", res);
-                    if res.status().is_success() {
-                        println!("Successful request to Discord");
-                        HttpResponse::Ok()
-                    } else {
-                        eprintln!("Not successful: {:?}", res);
-                        HttpResponse::InternalServerError()
-                    }
-                }
-                Err(_) => {
-                    eprintln!("Failed to send request to Discord");
-                    HttpResponse::InternalServerError()
-                }
+    let client = reqwest::Client::new();
+    let body = r#"
+                {
+                    "content": "",
+                    "tts": false,
+                    "embeds": [
+                        {
+                        "id": 10674342,
+                        "title": "🔥 New vote! 🔥",
+                        "description": "<@{{userId}}> just voted for <@{{botId}}>!\nYou can vote again <t:{{timeStamp}}:R>!",
+                        "color": 2326507,
+                        "fields": []
+                        }
+                    ],
+                    "components": [],
+                    "actions": {},
+                    "username": "Wumpus.store",
+                    "avatar_url": "https://cdn.discordapp.com/avatars/1207368481977147443/980c30c5c2e7896201f655a13af1037f.webp?size=80"
+                }"#
+                .replace("{{userId}}", data.userId.as_str())
+                .replace("{{botId}}", data.botId.as_str())
+                .replace("{{timeStamp}}", (Utc::now() + Duration::hours(12)).timestamp().to_string().as_str());
+
+    match client
+        .post(&query.webhook)
+        .body(body)
+        .header("content-type", "application/json")
+        .send()
+        .await
+    {
+        Ok(res) => {
+            if res.status().is_success() {
+                println!("Successful request to Discord");
+                HttpResponse::Ok()
+            } else {
+                eprintln!("Not successful: {:#?}", res);
+                HttpResponse::InternalServerError()
             }
-        } else {
-            eprintln!("Invalid authorization header, aborting.");
-            HttpResponse::Unauthorized()
         }
-    } else {
-        eprintln!("No authorization header, aborting.");
-        HttpResponse::Unauthorized()
+        Err(_) => {
+            eprintln!("Failed to send request to Discord");
+            HttpResponse::InternalServerError()
+        }
     }
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let env = Env {
-        discord_webhook: std::env::var("DISCORD_WEBHOOK").expect("DISCORD_WEBHOOK is not set"),
-        wumpus_token: std::env::var("WUMPUS_TOKEN").expect("WUMPUS_TOKEN is not set"),
-        webhook_body: std::env::var("WEBHOOK_BODY").unwrap_or(
-            r#"
-{
-  "content": "",
-  "tts": false,
-  "embeds": [
-    {
-      "id": 10674342,
-      "title": "🔥 New vote! 🔥",
-      "description": "<@{{userId}}> just voted for <@{{botId}}>!",
-      "color": 2326507,
-      "fields": []
-    }
-  ],
-  "components": [],
-  "actions": {},
-  "username": "wumpus.store",
-  "avatar_url": "https://wumpus.store/assets/icon.svg"
-}"#
-            .to_string(),
-        ),
+#[shuttle_runtime::main]
+async fn main() -> ShuttleActixWeb<impl FnOnce(&mut web::ServiceConfig) + Send + Clone + 'static> {
+    let config = move |cfg: &mut web::ServiceConfig| {
+        cfg.service(handle_webhook);
     };
 
-    HttpServer::new(move || {
-        App::new()
-            .app_data(Data::new(env.clone()))
-            .service(handle_webhook)
-    })
-    .bind(("0.0.0.0", 4056))?
-    .run()
-    .await
+    Ok(config.into())
 }
